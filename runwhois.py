825990e6737fce97fe21f395630d4b2eff1b8d0a
jeffmaddocks <jeff.maddocks@gmail.com>
2023-06-18 12:50:32 -0500

    keying the columns from the whois package

--- runwhois.py
+++ runwhois.py
@@ -2,21 +2,13 @@ import pandas as pd
 import whois
 
 def runwhois(df):
-    arfields = ['ip', 'org', 'name', 'domain_name', 'address', 'city', 'state', 'registrant_postal_code', 'country']
+    arfields=list(whois.whois(df[0][0]))  # grab the column names from the whois package using a whois lookup of the first record
     df_final = pd.DataFrame(columns = arfields)
-    arlookup = []
     for index, x in df.iterrows():  
-        arlookup = whois.whois(x[0])
-        try:
-            data = [str(x[0]), str(arlookup[arfields[1]]), str(arlookup[arfields[2]]), str(arlookup[arfields[3]]), str(arlookup[arfields[4]]), str(arlookup[arfields[5]]), str(arlookup[arfields[6]]), str(arlookup[arfields[7]]), str(arlookup[arfields[8]])]
-            df_final.loc[index] = data
-        except:
-            print('error row index ' + str(index) + ': ' + str(IOError))
+        df_final.loc[x[0]] = whois.whois(x[0])
     return df_final
 
 if __name__ == "__main__":
     df = pd.read_csv('trace.txt', header=None)
-    g = open('data.txt','w')
     df_final = runwhois(df)
-    g.write(df_final.to_csv(index=False))
-    g.close()
+    df_final.to_json('data.json', orient='index', indent=2 )
